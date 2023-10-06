81f9d6ccdd704285121e66acaa2953adbcae3226
SidraOsman <osmansidra4@gmail.com>
2023-10-06 09:57:21 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names =['Guadalupe' , 'Ollie' , 'Aki'];
+
+function writeCards (names){
+    let messages =[];
+    for (let i = 0; i < names.length; i++){
+        messages.push('Thank you, ${names[i]}, for the wonderful surprise gift!');
+    }
+    return messages; 
+}
+const thankYouMessages = writeCards(names);
+//console.log(thankYouMessages);
+
+
+function countDown(num) {
+    while(num>=0){
+        console.log(num);
+        num--;
+    }
+}
+
